80251105bb451e5e16250b2b68b29d511ea4fc8d
JoseNivaldo <silva.santos.jose2611@escola.pr.gov.br>
2025-08-13 23:18:02 -0300

    Add files via upload

---
+++ Leticia 2ºB Flahscards/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao (
+    'Espanhol',
+    'Qual o espanhol mais falado no mundo?',
+    'O México ficando em primeiro lugar, com cerca de 129 milhões de pessoas que falam o idioma.'
+)
+criaCartao (
+    'Ingles',
+    'Qual o ingles mais usado no mundo?',
+    'O inglês americano é o inglês mais usado no mundo.'
+)
+criaCartao (
+    'Geografia',
+    'qual a taxa de natalidade do brasil atualmente?',
+    'A taxa ao longo dos anos vem caindo e atualmente está com 1,55 filho por mulher.'
+)
+criaCartao (
+    'Biologia',
+    'Qual a cor mais abundante de gatos?',
+    'A cor de gato mais comum é o malhado, conhecida como tigrado ou rajado.'
+)
+
+
